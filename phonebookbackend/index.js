62b901ed8dc9728ffe37f96e7c19c56d31782e65
Pethaf <pethaf1@gmail.com>
2023-12-19 16:37:30 +0100

    Exercise 3.13
    Migrated fetching of entries from in-file database to MongoDB

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -1,6 +1,8 @@
 const http = require('http')
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config()
+const Person = require("./models/person")
 const app = express()
 app.use(express.static('dist'))
 const morgan = require("morgan");
@@ -8,10 +10,6 @@ morgan.token('type', function (req, res) { return req.body? JSON.stringify(res.b
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 app.use(express.json())
 app.use(cors())
-const generateId = () => {
-  return persons.length === 0 ? 1 : 
-   Math.max(...persons.map(person => person.id))+1
- }
  const requestLogger = (request, response, next) => {
    console.log('Method:', request.method)
    console.log('Path:  ', request.path)
@@ -20,58 +18,35 @@ const generateId = () => {
    next()
  }
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-
 
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(requestLogger)
 
-
 app.get("/api/persons",(req,res) => {
-    res.json(persons);
+    Person.find({}).then(result => {
+      res.json(result)
+    })
 })
 app.get("/info",(req,res)=> {
     const now = new Date()
-    res.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
+    Person.find({}).then(result => {
+      res.send( 
+        `<p>Phonebook has info for ${result.length} people</p>
         <p>${now.toString()}</p>`
-    )
+      )
+    })
 })
 app.get("/api/person/:id",(req,res) => {
-    const id = Number(req.params.id);
-    const person = persons.find(person => person.id === id);
-    if(!person){
-        res.status(404).end();
-    }
-    else {
-        res.json(person).status(200)
-    }
-})
+  Person.findById(req.params.id).then(result => {
+   if(result == null){
+    res.status(404).end()
+   }
+    res.json(result)
+  }).catch(error => console.log(error.message))}) 
+
 app.delete("/api/person/:id",(req,res)=>{
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id )
