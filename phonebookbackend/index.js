7297a227fba1e8c0db97c65f56b6a04d05ba7812
Pethaf <pethaf1@gmail.com>
2023-12-22 10:26:31 +0100

    Update index.js
    Clean up code

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -6,17 +6,17 @@ const Person = require("./models/person")
 const app = express()
 app.use(express.static('dist'))
 const morgan = require("morgan");
-morgan.token('type', function (req, res) { return req.body? JSON.stringify(res.body): "" })
+morgan.token('type', function (req, res) { return req.body ? JSON.stringify(res.body) : "" })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 app.use(express.json())
 app.use(cors())
- const requestLogger = (request, response, next) => {
-   console.log('Method:', request.method)
-   console.log('Path:  ', request.path)
-   console.log('Body:  ', request.body)
-   console.log('---')
-   next()
- }
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
 
 
@@ -25,38 +25,39 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(requestLogger)
 
-app.get("/api/persons",(req,res) => {
-    Person.find({}).then(result => {
-      res.json(result)
-    })
-})
-app.get("/info",(req,res)=> {
-    const now = new Date()
-    Person.find({}).then(result => {
-      res.send( 
-        `<p>Phonebook has info for ${result.length} people</p>
-        <p>${now.toString()}</p>`
-      )
-    })
-})
-app.get("/api/person/:id",(req,res) => {
-  Person.findById(req.params.id).then(result => {
-   if(result == null){
-    res.status(404).end()
-   }
+app.get("/api/persons", (req, res) => {
+  Person.find({}).then(result => {
     res.json(result)
-  }).catch(error => console.log(error.message))}) 
-
-app.delete("/api/person/:id",(req,res)=>{
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id )
-  res.status(204).end()
+  })
 })
-app.post("/api/persons/",(req, res) => {
+app.get("/info", (req, res) => {
+  const now = new Date()
+  Person.where({}).countDocuments().then(result => {
+    res.send(
+      `<p>Phonebook has info for ${result} people</p>
+        <p>${now.toString()}</p>`
+    )
+  })
+})
+app.get("/api/person/:id", (req, res) => {
+  Person.findById(req.params.id).then(result => {
+    if (result == null) {
+      res.status(404).end()
+    }
+    res.json(result)
+  }).catch(error => console.log(error.message))
+})
+
+app.delete("/api/person/:id", (req, res) => {
+  Person.findByIdAndDelete(req.params.id).then(result => {
+    res.status(204).end()
+  }).catch(error => next(error))
+})
+app.post("/api/persons/", (req, res) => {
   const body = req.body;
-  if(!body.name || ! body.phoneNumber){
+  if (!body.name || !body.phoneNumber) {
     return res.status(400).json({
-      error:'content missing'
+      error: 'content missing'
     })
   }
   const person = new Person({
